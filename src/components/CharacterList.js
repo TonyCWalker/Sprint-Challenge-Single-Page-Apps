1a76e518cf35491410fcbba9e9dc31135c7a59fd
Tony Walker <Walkertony509@gmail.com>
2019-12-15 00:13:32 -0800

    I have added routing to character/homepage.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -10,7 +13,11 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
+      <SearchForm />
       <h2>TODO: `array.map()` over your state here!</h2>
+      <Nav>
+        <Link to="/">Home</Link>
+      </Nav>
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const WelcomeWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  margin-top: 20px;
+`;
 
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
+    <WelcomeWrapper>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
@@ -11,6 +23,9 @@ export default function WelcomePage() {
           alt="rick"
         />
       </header>
-    </section>
+      <Nav>
+        <Link to="/characters">Characters</Link>
+      </Nav>
+    </WelcomeWrapper>
   );
 }
